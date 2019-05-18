260edca6ec4f750753600477ea4f472f19241064
phil <pslinge144@gmail.com>
2019-05-18 19:56:16 -0400

    Fixed script to extract tar.gz and zip files from USGS

--- io/pull_rois.py
+++ io/pull_rois.py
@@ -12,10 +12,16 @@ if __name__ == "__main__":
     for sensor_name in os.listdir(site_dir):
         print("\nReading from", sensor_name, "\n")
         sensor_dir = os.path.join(site_dir, sensor_name)
-        for filename in os.listdir(sensor_dir):
+        filenames = [f for f in os.listdir(sensor_dir) if
+                not os.path.isdir(os.path.join(sensor_dir, f))]
+        for filename in filenames:
             sensor_file = os.path.join(sensor_dir, filename)
             out_sensor_dir = os.path.splitext(sensor_file)[0]
             if not os.path.exists(out_sensor_dir):
-                sensor_file_extracted = shutil.unpack_archive(sensor_file,
-                        out_sensor_dir)
+                print("\t" + filename)
+                try:
+                    sensor_file_extracted = shutil.unpack_archive(sensor_file,
+                            out_sensor_dir)
+                except EOFError:
+                    print("\t\t-----SKIPPED------")
 
